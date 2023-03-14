a9abfc216e6ec4803f00e64dd882a2e6e62e70d7
caovanhoang63 <22520457@gm.uit.edu.vn>
2023-03-14 15:40:36 +0700

    alooo

--- Bai002/Bai002.cpp
+++ Bai002/Bai002.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
-	cout << "Hello world";
+	float r;
+	cout << "Nhap ban tinh ";
+	cin >> r;
+	float dt = 3.14 * r * r;
+	cout <<"Dien tich la " <<  dt;
+	return 0;
 	return 0;
 }
